c77d1caae338508754f98158cb27bb62d2586f89
Thuyan Duong <thuyan.duong@flatironschool.com>
2018-10-21 22:18:23 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -24,8 +24,8 @@ def move(board, index, char = "X")
   board[index] = char
 end
 
-def turn 
-  board = [" "," "," "," "," "," "," "," "," "]
+def turn(board)
+  # board = [" "," "," "," "," "," "," "," "," "]
   puts "Please enter 1-9:"
   input = gets.strip
   index = input_to_index(input)
