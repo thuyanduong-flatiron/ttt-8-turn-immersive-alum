c794534b059a8eb144eb0b93e08537fec3d6d0e3
Thuyan Duong <thuyan.duong@flatironschool.com>
2018-10-21 22:17:45 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -25,10 +25,15 @@ def move(board, index, char = "X")
 end
 
 def turn 
+  board = [" "," "," "," "," "," "," "," "," "]
   puts "Please enter 1-9:"
   input = gets.strip
   index = input_to_index(input)
   until valid_move?(board, index)
-  puts "Please enter 1-9:"
-  input = gets.strip
+    puts "Please enter 1-9:"
+    input = gets.strip
+    index = input_to_index(input)
+  end
+  move(board, index)
+  display_board(board)
 end
